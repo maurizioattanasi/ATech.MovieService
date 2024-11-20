c564f06681a97e22f6a5f69225bc1949a46af897
Maurizio Attanasi <maurizio.attanasi@gmail.com>
2024-11-20 22:29:22 +0100

    added compression

--- src/ATech.MovieService.Api/DependencyInjection.cs
+++ src/ATech.MovieService.Api/DependencyInjection.cs
@@ -1,17 +1,21 @@
 using System;
+using System.IO.Compression;
 
 using FastEndpoints;
 using FastEndpoints.Swagger;
+
+using Microsoft.AspNetCore.ResponseCompression;
 
 namespace ATech.MovieService.Api;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    public static IServiceCollection AddPresentation(this IServiceCollection services, IConfiguration configuration)
     {
         services
             .AddAuthorization()
             .AddFastEndpoints()
+            .AddCompression(configuration)
             .SwaggerDocument(opt =>
             {
                 opt.DocumentSettings = s =>
@@ -23,6 +27,30 @@ public static class DependencyInjection
                 opt.AutoTagPathSegmentIndex = 0;
             })
             ;
+
+        return services;
+    }
+
+    private static IServiceCollection AddCompression(this IServiceCollection services, IConfiguration configuration)
+    {
+        services
+            .AddResponseCompression(options =>
+            {
+                options.EnableForHttps = true;
+                options.Providers.Add<BrotliCompressionProvider>();
+                options.Providers.Add<GzipCompressionProvider>();
+            });
+
+        services
+            .Configure<BrotliCompressionProviderOptions>(options =>
+            {
+                options.Level = CompressionLevel.Fastest;
+            })
+            .Configure<GzipCompressionProviderOptions>(options =>
+            {
+                options.Level = CompressionLevel.SmallestSize;
+            });
+            
 
         return services;
     }
