f6514b9f94d4caf740063270dac43b899640d992
Maurizio Attanasi <maurizio.attanasi@gmail.com>
2024-12-21 18:52:04 +0100

    tidying up

--- src/ATech.MovieService.Api/Movies/Delete/DeleteMovieEndpoint.cs
+++ src/ATech.MovieService.Api/Movies/Delete/DeleteMovieEndpoint.cs
@@ -36,37 +36,3 @@ public class DeleteMovieEndpoint : IEndpoint
         }
     }
 }
-
-// public class DeleteMovieEndpoint(IMediator mediator, ILogger<DeleteMovieEndpoint> logger) : Endpoint<DeleteMovieRequest, EmptyResponse>
-// {
-//     public override void Configure()
-//     {
-//         Delete("api/movies/{id}");
-
-//         Options(x => x.WithTags("Movies"));
-
-//         AllowAnonymous(Http.DELETE);
-//     }
-
-//     public override async Task HandleAsync(DeleteMovieRequest req, CancellationToken ct)
-//     {
-//         try
-//         {
-//             await mediator.Send(new DeleteMovieCommand(req.Id), ct);
-
-//             Response = new EmptyResponse();
-//         }
-//         catch (ItemNotFoundException)
-//         {
-//             await SendNotFoundAsync(ct);
-//         }
-//         catch (Exception ex)
-//         {
-//             // Handle exception
-//             logger.LogError(ex, "An error occurred while deleting a movie.");
-
-//             // Return HTTP 500 Internal Server Error
-//             await SendErrorsAsync(StatusCodes.Status500InternalServerError, ct);
-//         }
-//     }
-// }
